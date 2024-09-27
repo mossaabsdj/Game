242a686814cc7ac892d389964b3b9a0cca5b8d21
mossaabsdj <saadsdj0@gmail.com>
2024-09-27 21:57:41 +0100

    gpt

--- app/Game/page.jsx
+++ app/Game/page.jsx
@@ -7,47 +7,43 @@ import ImageSplitter from "./test/page.jsx"; // Ensure this is a valid component
 import { Howl } from "howler";
 import { useEffect, useRef, useState } from "react";
 const Swal = require("sweetalert2");
-import React from "react";
-import { title } from "process";
 
 const letters = "abcdefghijklmnopqrstuvwxyz".split("");
-const SoundNext = new Howl({
-  src: ["/next.mp3"],
-});
-
-// Call the function with your image source
+const SoundNext = new Howl({ src: ["/next.mp3"] });
 
 function BTN(event) {
   const buttonText = event.currentTarget.textContent;
   console.log(buttonText);
   return buttonText;
 }
+
 function win() {
   return Swal.fire({
     title: "Congratulations!",
-    text: "you are super fan!",
+    text: "You are a super fan!",
     width: 600,
     padding: "3em",
     color: "#fff",
     background: "#4caf50 url(/images/graduation.png)",
     backdrop: `
-    rgba(76, 175, 80, 0.4)
-    url("/images/celebrate.gif")
-    center top
-    no-repeat
-  `,
+      rgba(76, 175, 80, 0.4)
+      url("/images/celebrate.gif")
+      center top
+      no-repeat
+    `,
     confirmButtonText: "Hooray!",
     confirmButtonColor: "#ff9800",
     icon: "success",
     imageUrl: "duncky.jpg",
   });
 }
+
 function Next_Stage(img, level) {
   return Swal.fire({
-    title: "Stage Done ",
+    title: "Stage Done",
     icon: "success",
     imageUrl: img[level],
-    confirmButtonText: `Next`,
+    confirmButtonText: "Next",
     allowOutsideClick: false, // Prevent closing by clicking outside
     preConfirm: (c) => {
       if (c) {
@@ -56,14 +52,16 @@ function Next_Stage(img, level) {
     },
   });
 }
+
 function lose() {
   return Swal.fire({
-    title: "You Lose ",
+    title: "You Lose",
     icon: "error",
-    confirmButtonText: `repeat`,
+    confirmButtonText: "Repeat",
     allowOutsideClick: false, // Prevent closing by clicking outside
   });
 }
+
 async function ArchiveName() {
   const newItem = { name: "itemName", value: "itemValue" };
 
@@ -77,37 +75,33 @@ async function ArchiveName() {
 
   if (response.ok) {
     console.log("Item added successfully");
-    setItemName("");
-    setItemValue("");
   } else {
     console.error("Failed to add item");
   }
 }
+
 function sweetalert(b, setb) {
   if (b === 1) {
+    return;
   } else {
     return Swal.fire({
       title: "Hello in Mossaab Game",
-      text: "Please Entre Votre Name",
+      text: "Please Enter Your Name",
       allowOutsideClick: false,
       input: "text",
-      inputAttributes: {
-        autocapitalize: "off",
-      },
+      inputAttributes: { autocapitalize: "off" },
       showCancelButton: false,
-      confirmButtonText: "Valider",
-
+      confirmButtonText: "Validate",
       showLoaderOnConfirm: true,
       preConfirm: (login) => {
         if (!login) {
           Swal.showValidationMessage("Name is required");
           return false;
         } else {
-          // ArchiveName();
           return Swal.fire({
-            title: "hi " + login,
+            title: "Hi " + login,
             icon: "success",
-            confirmButtonText: `start  first stage`,
+            confirmButtonText: "Start First Stage",
             allowOutsideClick: false, // Prevent closing by clicking outside
           }).then((result) => {
             if (result.isConfirmed) {
@@ -119,20 +113,13 @@ function sweetalert(b, setb) {
     });
   }
 }
+
 function Stage(Word, l) {
   var len = Word.length;
-  var WordC;
-  var tentative;
-  if (!sessionStorage.getItem("tentative")) {
-    sessionStorage.setItem("tentative", 3);
-  }
-  if (sessionStorage.getItem("WordC")) {
-    WordC = sessionStorage.getItem("WordC");
-  } else {
-    WordC = "*".repeat(len);
-  }
+  var WordC = sessionStorage.getItem("WordC") || "*".repeat(len);
+  var tentative = sessionStorage.getItem("tentative") || 3;
 
-  for (let i = 0; i <= len; i++) {
+  for (let i = 0; i < len; i++) {
     if (l === Word[i]) {
       WordC[i] = Word[i];
       break;
@@ -152,45 +139,34 @@ function Stage(Word, l) {
 }
 
 function Game() {
-  var defaultarray = [0, 1, 2, 3, 4, 5, 6, 7];
   const [find, setfind] = useState(false);
-  const [array, setarray] = useState([0, 1, 2, 3, 4, 5, 6, 7]);
-  const [random, setrandom] = useState(null);
-  const containerRef = useRef(null);
-  const maxtentativ = 7;
-  const DangerTentative = 3;
-  var found = false;
+  const [array, setarray] = useState([0, 1, 2, 3]);
+  const [random, setrandom] = useState(0);
   const [b, setb] = useState(0);
   const [level, setlevel] = useState(0);
   const [tentative_effect, settentative_effect] = useState("");
-  var words = ["librarys", "swimming", "onepiece", "mossaabsdj"];
-  var geusses = [
-    "Someone's  Job Places ",
-    "Someone's sport",
-    "Someone's Best Anime",
-    "best person in the world",
-  ];
-  var img = ["download.jpg", "swimming.jpg", "onepiece.jpg", "mossaab.jpg"];
-  var row = [4, 4, 4, 5];
-  var col = [2, 2, 2, 2];
 
-  var Scale = [0.9, 0.9, 0.7, 0.2];
+  const words = ["librarys", "swimming", "onepiece", "mossaabsdj"];
+  const geusses = [
+    "Someone's Job Places",
+    "Someone's Sport",
+    "Someone's Best Anime",
+    "Best Person in the World",
+  ];
+  const img = ["download.jpg", "swimming.jpg", "onepiece.jpg", "mossaab.jpg"];
+  const row = [4, 4, 4, 5];
+  const col = [2, 2, 2, 2];
+  const Scale = [0.9, 0.9, 0.7, 0.2];
   const [word, setword] = useState(words[level]);
   const [guess, setguess] = useState(geusses[level]);
   const prevWordc = useRef();
   const [wordc, setwordc] = useState("*".repeat(word.length));
-  const [tentative, settentative] = useState(maxtentativ);
-  const SoundIncorrect = new Howl({
-    src: [IncorrectSound],
-  });
-  const Soundcorrect = new Howl({
-    src: ["/win.mp3"],
-  });
-  const SoundNext = new Howl({
-    src: ["/next.mp3"],
-  });
+  const [tentative, settentative] = useState(7);
+  const SoundIncorrect = new Howl({ src: [IncorrectSound] });
+  const Soundcorrect = new Howl({ src: ["/win.mp3"] });
+
   useEffect(() => {
-    if (find === true) {
+    if (find) {
       let randomIndex = Math.floor(Math.random() * array.length);
       let randomNumber = array[randomIndex];
       array.splice(randomIndex, 1);
@@ -198,17 +174,9 @@ function Game() {
       setrandom(randomNumber);
     }
   }, [find]);
-  useEffect(() => {
-    let arrayy = [];
-    let l = words[level].length;
-    console.log("word[level].length" + words[level]);
-    for (let i = 0; i < l; i++) {
-      arrayy.push(i);
-    }
-    console.log("array level" + arrayy);
-    setarray(arrayy);
 
-    console.log("image" + img[0]);
+  useEffect(() => {
+    setarray(Array.from(Array(words[level].length).keys()));
     setword(words[level]);
     setwordc("*".repeat(words[level].length));
     setguess(geusses[level]);
@@ -216,13 +184,13 @@ function Game() {
 
   useEffect(() => {
     const container = document.getElementById("wordc");
-
     container.innerHTML = "";
+
     wordc.split("").forEach((letter, index) => {
       const span = document.createElement("span");
       span.textContent = letter;
       span.className = style.true;
-      span.style.setProperty("--delay", index); // Set delay for each letter
+      span.style.setProperty("--delay", index);
       container.appendChild(span);
     });
 
@@ -231,147 +199,115 @@ function Game() {
         win();
       } else {
         setlevel(level + 1);
-        //console.log("img level" + img[level]);
         Next_Stage(img, level);
       }
     }
-    console.log(tentative + "------" + wordc);
   }, [wordc]);
+
   useEffect(() => {
     settentative_effect(style.tentative);
 
     if (tentative === 0) {
       lose();
-      settentative(maxtentativ);
+      settentative(7);
       setlevel(0);
       setwordc("*".repeat(word.length));
     }
-    if (tentative != maxtentativ) {
+
+    if (tentative < 7) {
       SoundIncorrect.play();
       const container = document.getElementById("wordc");
       container.innerHTML = "";
       wordc.split("").forEach((letter, index) => {
         const span = document.createElement("span");
         span.className = style.false;
-
         span.textContent = letter;
-
-        span.style.setProperty("--delay", index); // Set delay for each letter
+        span.style.setProperty("--delay", index);
         container.appendChild(span);
       });
     }
-    if (tentative <= DangerTentative) {
+
+    if (tentative <= 3) {
       settentative_effect(style.tentativeDanger);
     } else {
       const timer = setTimeout(() => {
         settentative_effect("");
-      }, 250); // Match the duration of the animation
+      }, 250);
       return () => clearTimeout(timer);
     }
-
-    console.log(tentative + "------" + wordc);
   }, [tentative]);
+
   function BTN(event) {
     let newwordc = wordc.split("");
     const buttonText = event.currentTarget.textContent;
+    let found = false;
 
-    for (let i = 0; i <= word.length; i++) {
-      if ((word[i] === buttonText) & (newwordc[i] != buttonText)) {
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === buttonText && newwordc[i] !== buttonText) {
         newwordc[i] = word[i];
         found = true;
         setfind(true);
         setwordc(newwordc.join(""));
         Soundcorrect.play();
-        setTimeout(() => {
-          setfind(false);
-        }, 5);
+        setTimeout(() => setfind(false), 5);
         break;
       }
     }
+
     if (!found) {
-      setwordc(wordc);
-      console.log("i will modifed tentative" + found);
       settentative(tentative - 1);
     }
   }
-  useEffect(() => {
-    // Get all div elements
-    const divs = document.getElementsByTagName("div");
-
-    // Check if there are any div elements
-    if (divs.length > 0) {
-      // Get the first div element
-      const div = divs[0];
-
-      // Apply the blur effect using CSS
-      div.style.filter = "blur(0px)"; // Adjust the blur amount as needed
-    }
-  }, [b]);
 
   useEffect(() => {
-    // Get all div elements
-    const divs = document.getElementsByTagName("div");
-
-    // Check if there are any div elements
-    if (divs.length > 0) {
-      // Get the first div element
-      const div = divs[0];
-
-      // Apply the blur effect using CSS
-      div.style.filter = "blur(5px)"; // Adjust the blur amount as needed
-    }
     sweetalert(b, setb);
   }, []);
 
   return (
-    <>
-      <div className={style.body}>
-        <div className={style.container}>
-          <div className={style.jeupendu}>
-            <Image src={GImage} />
-            <h1>JEU DU PENDU</h1>
+    <div className={style.body}>
+      <div className={style.container}>
+        <div className={style.jeupendu}>
+          <Image src={GImage} />
+          <h1>JEU DU PENDU</h1>
+        </div>
+        <div className={style.gamebox}>
+          <div className={style.worddisplay} id="word-display">
+            <div id="s"></div>
           </div>
-          <div className={style.gamebox}>
-            <div className={style.worddisplay} id="word-display">
-              <div id="s"></div>
-            </div>
 
-            <div className={style.Guess}>
-              <h3 className={style.h3}>Guess: </h3>{" "}
-              <ImageSplitter
-                colss={col[level]}
-                rowss={row[level]}
-                imgg={img[level]}
-                scalee={Scale[level]}
-                random={random}
-              />
-              <h4 className={style.h4}>{guess}</h4>
-            </div>
+          <div className={style.Guess}>
+            <h3 className={style.h3}>Guess:</h3>
+            <ImageSplitter
+              colss={col[level]}
+              rowss={row[level]}
+              imgg={img[level]}
+              scalee={Scale[level]}
+              random={random}
+            />
+            <h4 className={style.h4}>{guess}</h4>
+          </div>
 
-            <h1 id="wordc" className={style.wordc}></h1>
-            <h4>
-              <span className={style.tentativetext}>
-                TENTATIVES INCORRECTES
-              </span>
-              <b>
-                <span className={tentative_effect}> {tentative}</span>/{" "}
-                {maxtentativ}
-              </b>
-            </h4>
-            <div className={style.keyboard}>
-              {letters.map((l) => (
-                <button onClick={BTN} key={l} className={style.btn}>
-                  {l}
-                </button>
-              ))}
+          <h1 id="wordc" className={style.wordc}></h1>
+          <h4>
+            <span className={style.tentativetext}>TENTATIVES INCORRECTES</span>
+            <b>
+              <span className={tentative_effect}> {tentative}</span>/ {7}
+            </b>
+          </h4>
+          <div className={style.keyboard}>
+            {letters.map((l) => (
+              <button onClick={BTN} key={l} className={style.btn}>
+                {l}
+              </button>
+            ))}
 
-              <button className={style.btn}>GO</button>
-              <br />
-            </div>
+            <button className={style.btn}>GO</button>
+            <br />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
+
 export default Game;
--- app/Game/test/page.jsx
+++ app/Game/test/page.jsx
@@ -2,32 +2,35 @@
 
 import { useEffect, useRef, useState } from "react";
 
-function ImageSplitter({ random, imgg, scalee, rowss, colss }) {
+const ImageSplitter = ({ random, imgg, scalee, rowss, colss }) => {
   const imageRef = useRef(null);
   const [img, setimg] = useState(null);
   const [scale, setscale] = useState(1);
   const [row, setrow] = useState(6);
   const [col, setcol] = useState(2);
+
   useEffect(() => {
     setcol(colss);
   }, [colss]);
+
   useEffect(() => {
     setscale(scalee);
   }, [scalee]);
+
   useEffect(() => {
     setrow(rowss);
   }, [rowss]);
+
   useEffect(() => {
-    console.log("imgg" + imgg);
     setimg(imgg);
   }, [imgg]);
+
   const unlock = (i) => {
     const canvas = document.getElementById("canvas-" + i);
     if (canvas) {
       const ctx = canvas.getContext("2d");
-
       const image = new Image();
-      image.src = img; // Replace with your image path
+      image.src = img;
 
       image.onload = () => {
         const rows = row; // Number of rows
@@ -57,18 +60,18 @@ function ImageSplitter({ random, imgg, scalee, rowss, colss }) {
   };
 
   useEffect(() => {
-    if (random === null) {
-    } else {
+    if (random !== null) {
       unlock(random);
     }
   }, [random]);
+
   useEffect(() => {
     const image = new Image();
     image.src = img;
 
     image.onload = () => {
-      const rows = row; // Number of rows
-      const cols = col; // Number of columns
+      const rows = row;
+      const cols = col;
       const scaleFactor = scale;
       const scaledWidth = (image.width * scaleFactor) / cols;
       const scaledHeight = (image.height * scaleFactor) / rows;
@@ -121,7 +124,7 @@ function ImageSplitter({ random, imgg, scalee, rowss, colss }) {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        height: "10vh", // Full viewport height for vertical centering
+        height: "10vh",
       }}
     >
       <div
@@ -133,6 +136,6 @@ function ImageSplitter({ random, imgg, scalee, rowss, colss }) {
       ></div>
     </div>
   );
-}
+};
 
 export default ImageSplitter;
